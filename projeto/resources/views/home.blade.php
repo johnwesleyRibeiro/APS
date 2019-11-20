df001a0430394da6f87755bf5597026b01e58746
johnwesleyRibeiro <johnwesley12123@gmail.com>
2019-11-20 03:00:12 -0300

    Criador o diretorio

---
+++ projeto/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div style="text-align: center; color:red;font:100px;">
+
+
+
+
+Home
+</div>
+
+
+
+
+
+@endsection
---
+++ projeto/resources/views/layouts/_rod.blade.php
@@ -0,0 +1,12 @@
+<br><br><br><br><br>
+
+
+<div class="rod container">
+<br><br><br>
+
+Programadores © Gean Lucas E John Wesley 2019
+<br><br><br>
+<div>
+ 
+</div>
+</div>
